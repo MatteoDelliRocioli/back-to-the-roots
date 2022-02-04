bd4404b2122c523ace66ca8a7b96d495ac7a19d2
Matteo Delli Rocioli <mdellirocioli@altitudo.com>
2022-02-04 07:21:41 +0100

    Convert vectors to actual arrays:
    + WIP: using hash tables for a better time complexity solution

--- ztm-algos-n-ds/section4/same-item-in-2-arrays/main.cpp
+++ ztm-algos-n-ds/section4/same-item-in-2-arrays/main.cpp
@@ -22,22 +22,34 @@ should return true;
 using namespace std;
 
 bool solQuadratic(
-	const vector<char> &array1, const vector<char> &array2);
+	char *array1, size_t arr1Size, char *array2, size_t arr2Size);
+
+//bool solConstant();
 
 int main()
 {
-	//The most bruteforce approach would be to iterate the two arrays and
-	// then return as soon there is a common item
-	// this would be a quadratic time solution and constant space solution
+	//char array1[] {'a', 'b', 'c', 'x'};
+	//char array2[] {'z', 'y', 'i'};
+	char array1[] {'a', 'b', 'c', 'x'};
+	char array2[] {'z', 'y', 'x'};
 
-	//const vector<char> array1 {'a', 'b', 'c', 'x'};
-	//const vector<char> array2 {'z', 'y', 'i'};
-	const vector<char> array1 {'a', 'b', 'c', 'x'};
-	const vector<char> array2 {'z', 'y', 'x'};
+	size_t array1Size = sizeof(array1)/sizeof(array1[0]);
+	size_t array2Size = sizeof(array2)/sizeof(array2[0]);
 
 	bool result {false};
 
-	result = solQuadratic(array1, array2);
+	/*The most bruteforce approach would be to iterate the two arrays and
+	 then return as soon there is a common item
+	 this would be a quadratic time solution and constant space solution*/
+	result = solQuadratic(array1, array1Size, array2, array2Size);
+
+	/*A more efficient approach would be to use a hash table, that would
+	 * mean that we have to loop the first array to populate the hash
+	 * table, then loop the second array to look for any match with first
+	 * array. Since the look up operation on hash table (unordered_map in
+	 * cpp) is constant time, then we would finally have two separate
+	 * loops meaning a O(a + b) solution time complexity*/
+	//result = solLinear();
 
 	if (result)
 	{
@@ -47,16 +59,17 @@ int main()
 	return 0;
 }
 
+
 bool solQuadratic(
-	const vector<char> &array1, const vector<char> &array2)
+	char *array1, size_t arr1Size, char *array2, size_t arr2Size)
 {
 	bool result {false};
 
-	for (auto x : array1)
+	for (size_t i {0}; i < arr1Size; i++)
 	{
-		for (auto y : array2)
+		for (size_t j {0}; j < arr2Size; j++)
 		{
-			if (x == y)
+			if (array1[i] == array2[j])
 			{
 				result = true;
 				return result;
@@ -67,3 +80,13 @@ bool solQuadratic(
 	return result;
 }
 
+//bool solLinear(vector<char> &array1, vector<char> &array2)
+//{
+	//bool result {false};
+
+	////We have to loop
+
+	//for (auto)
+
+	//return result;
+//}
