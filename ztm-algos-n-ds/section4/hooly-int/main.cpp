7895d7d853f8d6183c8ddc3c80e1a2c30aa20c2d
Matteo Delli Rocioli <mdellirocioli@altitudo.com>
2022-02-05 15:53:05 +0100

    Write the recommendations

--- ztm-algos-n-ds/section4/hooly-int/main.cpp
+++ ztm-algos-n-ds/section4/hooly-int/main.cpp
@@ -1,11 +1,48 @@
 //https://www.youtube.com/watch?v=XKu_SEDAykw
 
+/* Take notes
+ * what are the inputs and the outputs?
+ * do I have time and space constraints?
+ * start with the naive brute approach, speack outloud to make your
+ 		tought process clear
+ * observe and tell why the brute approach is not the best (i.e. O(n^2)
+ 		algorithm or higher, not readable etc...)
+ * check your code for possible bugs (think about edge cases)
+ * did you use all the information provided?
+ * focus on the bottleneck (code with the greatest big O)
+ *
+ * while you code write down the steps you are going to follow
+ * modularize your code right from the beginning
+ * never start writing code unless you are sure how the things are going
+ 		to work
+ * start with the easy part
+ * tell that you would now write test to see if the code works properly
+ * use meaningful variable names
+ * go through your code outloud to test it live
+ * propose different solutions, talk about what you would google, other
+		interesting solutions that you didn't think about?
+ * how would you change the solution if you would have to deal with
+ 		large inputs (too large to stay in memory)? // divide and conquer
+ 		approach
+ * Communicate your thought process as much as possible
+ * */
+
+ /* The goal is to find a pair present in the array where the sum of
+ * those is equal to a provided amount
+ *
+ * data sets:
+ *
+ * [1, 2, 3, 9] Sum = 8 -> no
+ * [1, 2, 4, 4] Sum = 8 -> yes
+ * */
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
+	int* array1 {1, 2, 3, 9};
+	i
 
 	return 0;
 }
