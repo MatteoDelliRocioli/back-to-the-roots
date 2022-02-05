372d8e7ea8ba3bd8e7bd21689f6c3941a1a5420d
Matteo Delli Rocioli <mdellirocioli@altitudo.com>
2022-02-05 17:50:09 +0100

    Implement linear solution

--- ztm-algos-n-ds/section4/hooly-int/main.cpp
+++ ztm-algos-n-ds/section4/hooly-int/main.cpp
@@ -35,14 +35,75 @@
  * [1, 2, 3, 9] Sum = 8 -> no
  * [1, 2, 4, 4] Sum = 8 -> yes
  * */
+
+/*A brute way of finding the solution would be to iterate the array two
+ * times, one to get the first number to sum and the second time to see
+ * if it can be summed up with any other element of the array to equal
+ * the expexted sum value
+ *
+ * Since the array is sorted, a more efficient way to do this would be
+ * to get two numbers, one called low and one called high.
+ * The first being the smallest number in the collection and the latter
+ * being the largest. Try to sum them to check if they sum up to more
+ * than the target sum, if sum too large then move down of one index the
+ * high otherwise move up of one index the low number.
+ * The will eventually sum up to the target or cross each other
+ * */
 #include <iostream>
 
 using namespace std;
 
+bool linearSolve(int *arr, int arrSize, int sumTarget);
+
 int main()
 {
-	int* array1 {1, 2, 3, 9};
-	i
+	int array1 [] {1, 2, 3, 9};
+	//int array1 [] {1, 2, 4, 4};
+	int sumTarget = 12;
+
+	size_t arrSize = sizeof(array1) / sizeof(array1[0]);
+
+	bool result = linearSolve(array1, arrSize, sumTarget);
+	//result = linearSolve(array2, sumTarget);
+
+	cout << boolalpha << result << endl;
 
 	return 0;
+}
+
+bool linearSolve(int *arr, int arrSize, int sumTarget)
+{
+	bool result {false};
+	int low = 0;
+	int high = arrSize - 1;
+
+	for (int i {0}; i < arrSize; i++)
+	{
+		int sum = arr[low] + arr[high];
+		cout << "sum: " << sum << endl;
+		cout << "sumTarget: " << sumTarget << endl;
+
+		if (sum == sumTarget)
+		{
+			result = true;
+			return result;
+		}
+
+		if (low == high)
+		{
+			return result;
+		}
+
+		if (sum < sumTarget)
+		{
+			low += 1;
+		}
+
+		if (sum > sumTarget)
+		{
+			high -= 1;
+		}
+	}
+
+	return result;
 }
