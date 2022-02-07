50131dfb6ce4627b029f5d24d15293642f22ad9f
Matteo Delli Rocioli <mdellirocioli@altitudo.com>
2022-02-07 22:10:21 +0100

    WIP: Merge two sorted arrays into one

---
+++ ztm-algos-n-ds/section6/merge-2-sorted-arrays/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+ * Given 2 sorted arrays merge them into one big sorted array
+ *
+ * Example data set:
+ * int [] {4, 6, 30};
+ * int [] {0, 3, 4, 31};
+ * */
+
+int main()
+{
+	return 0;
+}
+
