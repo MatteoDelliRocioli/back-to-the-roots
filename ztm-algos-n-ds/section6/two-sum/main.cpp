99ac71a3c7cc29e47d1628d1737edd9dbf25340a
Matteo Delli Rocioli <mdellirocioli@altitudo.com>
2022-02-10 22:53:27 +0100

    Update two sum solution to take vector as input

--- ztm-algos-n-ds/section6/two-sum/main.cpp
+++ ztm-algos-n-ds/section6/two-sum/main.cpp
@@ -20,21 +20,65 @@ vector<int> findPairSumUpToTarget(int *arr1, int aS, int target);
 void printArr(int *arr1, int size);
 void printVector(vector<int> vec);
 
+vector<int> twoSum(vector<int>& nums, int target);
+
 int main()
 {
 	//int arr1 [] {};
-	int arr1 [] {1, 2, 7, 9, 4, 5, 3};
-	int target {12};
+	int arr1 [] {3, 2, 4};
+	int target {6};
 	int arr1Size = sizeof(arr1) / sizeof(arr1[0]);
+
+	vector<int> vec1 {3, 3};
 
 	printArr(arr1, arr1Size);
 
-	vector<int> result = findPairSumUpToTarget(arr1, arr1Size, target);
+	//vector<int> result = findPairSumUpToTarget(arr1, arr1Size, target);
+	vector<int> result = twoSum(vec1, target);
 
 	cout << "result: ";
 	printVector(result);
 
 	return 0;
+}
+
+vector<int> twoSum(vector<int>& nums, int target)
+{
+	vector<int> result {};
+
+	unordered_map<int, int> compMap{{}};
+	compMap.insert({nums.at(0), 0});
+
+	for (int i {1}; i < static_cast<int>(nums.size()); i++)
+	{
+		cout << "i: " << i << endl;
+
+		compMap.insert({nums.at(i), i});
+
+		int complement = target - nums.at(i);
+
+		cout << "complement: " << complement << endl;
+
+		auto got = compMap.find(complement);
+
+		if (got != compMap.end())
+		{
+			cout << "got->first: " << (*got).first << endl;
+			cout << "got->second: " << got->second << endl;
+			cout << "arr1[i]: " << nums.at(i) << endl;
+			cout << "i: " << i << endl;
+		}
+
+		if (got != compMap.end())
+		{
+			result.push_back((*got).second);
+			result.push_back(i);
+
+			return result;
+		}
+	}
+
+	return result;
 }
 
 vector<int> findPairSumUpToTarget(int *arr1, int aS, int target)
