6593d11987c5a997c4d961b16825f6cf891f38db
Matteo Delli Rocioli <mdellirocioli@altitudo.com>
2022-02-14 00:05:05 +0100

    Update handle edge cases

--- ztm-algos-n-ds/section6/maximum-subarray/main.cpp
+++ ztm-algos-n-ds/section6/maximum-subarray/main.cpp
@@ -6,7 +6,6 @@
 using namespace std;
 
 int maxSubArray(vector<int>& nums);
-int maxSubArray_01(vector<int>& nums);
 int maxSubArray_02(vector<int>& nums);
 void printpositiveStreakFirstElements(unordered_map<int, int> & map);
 void printVector(vector<int>& nums);
@@ -14,22 +13,131 @@ void printVector(vector<int>& nums);
 int main()
 {
 	vector<int> vec {-2,1,-3,4,-1,2,1,-5,4};
-	//vector<int> vec {1};
+	//vector<int> vec {0};
 	//vector<int> vec {-2,1,-3,4,-1,2,1,-5,-11,4};
 	//vector<int> vec {5,4,-1,7,8};
 	//vector<int> vec {-2,1,-3,4,-1,2,1,-5,4, -2, 6};
+	//vector<int> vec {-6};
 
 	printVector(vec);
 
 	//int result = maxSubArray(vec);
 	//int result = maxSubArray_01(vec);
-	int result = maxSubArray_02(vec);
+	int result = maxSubArray(vec);
 
 	cout << "result: " << result << endl;
 
 	cin.get();
 
 	return 0;
+}
+
+int maxSubArray(vector<int>& nums)
+{
+	int i {0};
+	int positiveSum {0};
+	int secondStreakPositiveSum {0};
+	int negativeStreakSum {0};
+	int largestSum {INT_MIN};
+	bool hasFoundMiddleNegatives {false};
+	int biggestNumber {INT_MIN};
+
+	if (nums.size() > 0)
+	{
+		largestSum = nums.at(0);
+		biggestNumber = nums.at(0);
+	}
+
+	while (i < static_cast<int>(nums.size()))
+	{
+		//loop positives
+		while(i < static_cast<int>(nums.size()) && nums.at(i) >= 0)
+		{
+			 //If we already found positive numbers then we compare last positive
+			 // streak sum with the next one
+			 if (positiveSum > 0 && hasFoundMiddleNegatives)
+			 {
+				  secondStreakPositiveSum += nums.at(i);
+			 }
+			 else
+			 {
+				  positiveSum += nums.at(i);
+			 }
+
+				if (nums.at(i) > biggestNumber)
+				{
+					biggestNumber = nums.at(i);
+				}
+			 i++;
+		}
+
+		//Check if summing extremes and middle negative numbers we obtain a
+		// result greater than each one of the single positive extremes
+		// taken alone
+		if (positiveSum > 0 || secondStreakPositiveSum > 0)
+		{
+			 int newLargest = positiveSum > secondStreakPositiveSum ?
+				  positiveSum : secondStreakPositiveSum;
+
+			 if (newLargest > largestSum)
+			 {
+				  largestSum = newLargest;
+			 }
+		}
+
+		int positivesAndNegatives =
+			 positiveSum + negativeStreakSum + secondStreakPositiveSum;
+
+		int newLargest = positivesAndNegatives;
+
+		if (newLargest > largestSum)
+		{
+				largestSum = newLargest;
+		}
+
+		if (positiveSum > positivesAndNegatives &&
+			positiveSum > secondStreakPositiveSum &&
+			positiveSum > largestSum)
+		{
+			largestSum = positiveSum;
+		}
+
+		if (secondStreakPositiveSum > positivesAndNegatives &&
+			secondStreakPositiveSum > positiveSum &&
+			secondStreakPositiveSum > largestSum)
+		{
+			largestSum = secondStreakPositiveSum;
+		}
+
+		if (positiveSum > 0 && secondStreakPositiveSum > 0)
+		{
+			positiveSum = secondStreakPositiveSum;
+			secondStreakPositiveSum = 0;
+		}
+
+		//loop negatives
+		hasFoundMiddleNegatives = false;
+		negativeStreakSum = 0;
+		while(i < static_cast<int>(nums.size()) && nums.at(i) < 0)
+		{
+			hasFoundMiddleNegatives = true;
+			negativeStreakSum += nums.at(i);
+			if (nums.at(i) > biggestNumber)
+			{
+				biggestNumber = nums.at(i);
+			}
+			i++;
+		}
+	}
+
+	if (biggestNumber != INT_MIN &&
+		biggestNumber < largestSum &&
+		biggestNumber <= 0)
+	{
+		largestSum = biggestNumber;
+	}
+
+	return largestSum;
 }
 
 int maxSubArray_02(vector<int>& nums)
