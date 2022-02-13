aea257d6210de1ffc1fbcf46dd10170a55341fdb
Matteo Delli Rocioli <mdellirocioli@altitudo.com>
2022-02-13 23:42:47 +0100

    Delete duplicated methods

--- ztm-algos-n-ds/section6/maximum-subarray/main.cpp
+++ ztm-algos-n-ds/section6/maximum-subarray/main.cpp
@@ -26,7 +26,7 @@ int main()
 	int result = maxSubArray_02(vec);
 
 	cout << "result: " << result << endl;
-	
+
 	cin.get();
 
 	return 0;
@@ -85,37 +85,37 @@ int maxSubArray_02(vector<int>& nums)
 
 		//Check if summing extremes and middle negative numbers we obtain a
 		// result greater than each one of the single positive extremes
-		// taken alone		
+		// taken alone
 		if (positiveSum > 0 || secondStreakPositiveSum > 0)
 		{
 			int newLargest = positiveSum > secondStreakPositiveSum ?
 				positiveSum : secondStreakPositiveSum;
-			
+
 			if (newLargest > largestSum)
 			{
 				largestSum = newLargest;
 			}
 		}
-		
+
 		int positivesAndNegatives =
 			positiveSum + negativeStreakSum + secondStreakPositiveSum;
 
 		int newLargest = positivesAndNegatives;
-		
+
 		if (newLargest > largestSum)
 		{
 			largestSum = newLargest;
 		}
 
 		if (positiveSum > positivesAndNegatives &&
-			positiveSum > secondStreakPositiveSum && 
+			positiveSum > secondStreakPositiveSum &&
 			positiveSum > largestSum)
 		{
 			largestSum = positiveSum;
 		}
 
 		if (secondStreakPositiveSum > positivesAndNegatives &&
-			secondStreakPositiveSum > positiveSum && 
+			secondStreakPositiveSum > positiveSum &&
 			secondStreakPositiveSum > largestSum)
 		{
 			largestSum = secondStreakPositiveSum;
@@ -146,176 +146,6 @@ int maxSubArray_02(vector<int>& nums)
 	}
 
 	return largestSum;
-}
-
-int maxSubArray_01(vector<int>& nums)
-{
-	int vecSize = (int) nums.size();
-	int low_left {0};
-	int high_left {0};
-
-	int tempSum {0};
-	int sumLeft {0};
-
-	vector<int> indexes {};
-
-	if (vecSize == 0)
-	{
-		return 0;
-	}
-
-	if (vecSize == 1)
-	{
-		return nums.at(0);
-	}
-
-	//int lowExtremeFirstElement = INT_MIN;
-	int highestNumber = nums.at(0);
-
-	for (int i {0}; i < vecSize; i++)
-	{
-//1st block
-		if (nums.at(i) > highestNumber)
-		{
-			highestNumber = nums.at(i);
-		}
-
-		if (nums.at(i) > 0)
-		{
-			low_left = i;
-
-			while(nums.at(i) > 0)
-			{
-				cout << "adding " << nums.at(i) << endl;
-				tempSum += nums.at(i);
-
-				if (i == vecSize - 1)
-				{
-					break;
-				}
-				i++;
-			}
-			sumLeft = tempSum;
-			tempSum = 0;
-		}
-
-		cout << endl << "sumLeft: " << sumLeft << endl;
-
-		cout << "low_left: (index | value) " << low_left << " | " <<
-			nums.at(low_left) << endl;
-		cout << "high_left: (index | value) " << high_left << " | " <<
-			nums.at(high_left) << endl;
-
-		cout << "nums.at(i) after seen positives: " << nums.at(i) << endl;
-
-//2nd block
-		while (nums.at(i) < 0)
-		{
-			cout << nums.at(i) << endl;
-			i++;
-		}
-
-		cout << "nums.at(i) after seen negatives: " << nums.at(i) << endl;
-
-//3rd block
-		//if (nums.at(i) > highestNumber)
-		//{
-			//highestNumber = nums.at(i);
-		//}
-
-		//if (nums.at(i) > 0)
-		//{
-			//low_left = i;
-
-			//while(nums.at(i) > 0)
-			//{
-				//high_left = i;
-				//if (i == vecSize - 1)
-				//{
-					//break;
-				//}
-				//i++;
-			//}
-		//}
-	}
-
-	cout << endl << "highestNumber: " << highestNumber << endl << endl;
-
-	return 0;
-}
-
-int maxSubArray(vector<int>& nums)
-{
-	int vecSize = (int) nums.size();
-
-	if (vecSize == 0)
-	{
-		return 0;
-	}
-
-	if (vecSize == 1)
-	{
-		return nums.at(0);
-	}
-
-	int lowExtremeFirstElement = INT_MIN;
-	//int highExtremeLastElement = INT_MIN;
-	int highestNumber = nums.at(0);
-
-	//nums value, nums index
-	//unordered_map<int, int> positiveStreakFirstElements;
-	//nums index, nums value
-	unordered_map<int, int> positiveStreakFirstElements;
-	//unordered_map<int, int> highExtremeLastElement;
-
-	for (int i {0}; i < vecSize; i++)
-	{
-		int currentElement = nums.at(i);
-
-		if(currentElement > highestNumber)
-		{
-			highestNumber = currentElement;
-		}
-
-		if (currentElement > 0)
-		{
-
-			cout << endl << "positiveStreakFirstElements.size(): " <<
-				positiveStreakFirstElements.size() << endl;
-
-			lowExtremeFirstElement = currentElement;
-
-			cout << "lowExtremeFirstElement: " << lowExtremeFirstElement
-				<< endl;
-
-			positiveStreakFirstElements[i] = lowExtremeFirstElement;
-
-			while(nums.at(i) > 0)
-			{
-				if (i == vecSize - 1)
-				{
-					break;
-				}
-				i++;
-			}
-
-			if (positiveStreakFirstElements.size() > 1 && i != vecSize - 1)
-			{
-				cout << "High extreme last number: " << nums.at(i - 1) << endl;
-			}
-
-		}
-
-		if (nums.at(i) < 0)
-		{
-			cout << nums.at(i) << endl;
-		}
-
-	}
-
-	printpositiveStreakFirstElements(positiveStreakFirstElements);
-
-	return 0;
 }
 
 void printpositiveStreakFirstElements(unordered_map<int, int>& map)
