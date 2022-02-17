456b6b2965c5c06db4b42d7611b2cf8ab3c561ab
Matteo Delli Rocioli <mdellirocioli@altitudo.com>
2022-02-17 22:27:33 +0100

    Implement contains duplicate solution

--- ztm-algos-n-ds/section6/contains-duplicate/main.cpp
+++ ztm-algos-n-ds/section6/contains-duplicate/main.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
+#include <vector>
+#include <unordered_set>
 
 using namespace std;
 
+bool containsDuplicate(vector<int>& nums);
+
 int main()
 {
+	vector<int> vec {1,1,1,3,3,4,3,2,4,2};
+
+	bool result = containsDuplicate(vec);
+
+	cout << boolalpha;
+	cout << result << endl;
+
 	return 0;
+}
+
+bool containsDuplicate(vector<int>& nums)
+{
+	unordered_set<int> uS;
+
+	for (int i {i}; i < (int)nums.size(); i++)
+	{
+		if (uS.find(nums.at(i)) != uS.end())
+		{
+			return true;
+		}
+		uS.insert(nums.at(i));
+	}
+
+	return false;
 }
