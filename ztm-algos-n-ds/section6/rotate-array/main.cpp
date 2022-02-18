fde1d7876cc4555055bbb6c180082155df077282
Matteo Delli Rocioli <mdellirocioli@altitudo.com>
2022-02-18 21:40:15 +0100

    Implement first naive solution for rotate array

--- ztm-algos-n-ds/section6/rotate-array/main.cpp
+++ ztm-algos-n-ds/section6/rotate-array/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -6,6 +7,11 @@ void rotate(vector<int>& nums, int k);
 
 int main()
 {
+	vector<int> nums {1,2,3,4,5,6,7};
+	int k {3};
+
+	rotate(nums, k);
+
 	return 0;
 }
 
@@ -17,5 +23,27 @@ int main()
 //That solution would be O(n) space complexity and O(n) time complexity
 void rotate(vector<int>& nums, int k)
 {
-	
+	vector<int> res;
+
+	for (int i {nums.size() - k}; i < nums.size(); i++)
+	{
+		cout << nums.at(i) << " ";
+		res.push_back(nums.at(i));
+	}
+
+	cout << endl << endl;
+
+	for (int i {0}; i < (nums.size() - k); i++)
+	{
+		cout << nums.at(i) << " ";
+		res.push_back(nums.at(i));
+	}
+
+	nums = res;
+
+	cout << endl << "res: ";
+	for (int x : nums)
+	{
+		cout << x << " ";
+	}
 }
