925f82f7b8ae6fc4d45cff8c5578a98fff861c8a
Matteo Delli Rocioli <mdellirocioli@altitudo.com>
2022-02-18 20:28:27 +0100

    Update with possible solution explaination

--- ztm-algos-n-ds/section6/rotate-array/main.cpp
+++ ztm-algos-n-ds/section6/rotate-array/main.cpp
@@ -2,7 +2,20 @@
 
 using namespace std;
 
+void rotate(vector<int>& nums, int k);
+
 int main()
 {
 	return 0;
+}
+
+
+//We can take the last k elements from the array and copy them into
+// a new array. The remaining part of the new array would be a copy of
+// the original array elements (index from 0 to arr.length - k)
+//
+//That solution would be O(n) space complexity and O(n) time complexity
+void rotate(vector<int>& nums, int k)
+{
+	
 }
