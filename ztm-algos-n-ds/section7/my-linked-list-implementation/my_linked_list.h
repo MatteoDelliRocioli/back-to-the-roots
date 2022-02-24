8a7720dccf21b48fe869d4c8a8e146e64a3dbc3a
Matteo Delli Rocioli <mdellirocioli@altitudo.com>
2022-02-24 22:48:33 +0100

    Update linked list assignment

---
+++ ztm-algos-n-ds/section7/my-linked-list-implementation/my_linked_list.h
@@ -0,0 +1,14 @@
+/*The linked list class will have to:
+- be hable to create a new empty linked list
+- have the following methods:
+	- insert(int value, int position = 0)
+	- remove(int position)
+	- get(int position)
+	- update(int value, int position)
+
+value1
+ hex1 --> value2
+ 					 hex2 --> value3
+ 					 					 hex3 --> etc... valueN
+ 					 					 								 nullptr
+*/
