b0868fc6fefb64287345a7bf342816a97f72fee0
Matteo Delli Rocioli <mdellirocioli@altitudo.com>
2022-03-07 21:59:41 +0100

    WIP: Crate node for binary tree implementation

---
+++ ztm-algos-n-ds/section-10/my-binary-tree-implementation/main.cc
@@ -0,0 +1,9 @@
+#include "binary_tree.h"
+#include "node.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  return 0;
+}
---
+++ ztm-algos-n-ds/section-10/my-binary-tree-implementation/node.h
@@ -0,0 +1,12 @@
+#pragma once
+
+class Node {
+  public:
+    Node* left;
+    Node* right;
+    int data;
+
+    Node();
+    Node(int data);
+    ~Node();
+};
