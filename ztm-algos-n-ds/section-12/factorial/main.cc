457de725300c9a87eb4676f12c71d5e688249781
Matteo Delli Rocioli <mdellirocioli@altitudo.com>
2022-03-16 23:38:29 +0100

    Init factorial

---
+++ ztm-algos-n-ds/section-12/factorial/main.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int FindFactorialRecursive(int number);
+int FindFactorialForLoop(int number);
+
+int main() {
+  return 0;
+}
+
+
+int FindFactorialRecursive(int number) {
+  return 0;
+}
+
+int FindFactorialForLoop(int number) {
+  return 0;
+}
