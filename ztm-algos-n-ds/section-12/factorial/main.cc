90b3a5ce472c2a7b63ac3f83910c0caf1123a58c
Matteo Delli Rocioli <mdellirocioli@altitudo.com>
2022-03-16 23:57:30 +0100

    Implement factorial for loop

--- ztm-algos-n-ds/section-12/factorial/main.cc
+++ ztm-algos-n-ds/section-12/factorial/main.cc
@@ -4,14 +4,44 @@ int FindFactorialRecursive(int number);
 int FindFactorialForLoop(int number);
 
 int main() {
+  int result {FindFactorialForLoop(7)};
+
+  std::cout << "result: " << result << std::endl;
+
+  int resultFromRecursion {FindFactorialRecursive(7)};
+
+  std::cout << "resultFromRecursion: " << resultFromRecursion << std::endl;
+
   return 0;
 }
 
 
 int FindFactorialRecursive(int number) {
+  if (number == 0) {
+    return 1;
+  }
+
+  int result = number;
+
+  if (number >= 1) {
+    return FindFactorialRecursive(number);
+  }
+
   return 0;
 }
 
 int FindFactorialForLoop(int number) {
-  return 0;
+  int result {1};
+
+  if (number == 0) {
+    return result;
+  }
+
+  while (number > 1) {
+    //std::cout << number << std::endl;
+    result *= number;
+    number--;
+  }
+
+  return result;
 }
