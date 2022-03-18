1c2604b6748678ba9b2012934721e0efd341f659
Matteo Delli Rocioli <mdellirocioli@altitudo.com>
2022-03-18 19:42:17 +0100

    Implement iterative solution for reverse string

--- ztm-algos-n-ds/section-12/reverse-string-with-recursion/main.cc
+++ ztm-algos-n-ds/section-12/reverse-string-with-recursion/main.cc
@@ -1,12 +1,34 @@
 #include <iostream>
 #include <string>
 
-void ReverseString(string str);
+std::string ReverseStringIterative(std::string str);
+void ReverseStringRecursive(std::string str);
 
 int main() {
+  std::string result = ReverseStringIterative("hey");
+
+  std::cout << result << std::endl;
+
   return 0;
 }
 
-void ReverseString(string str) {
+std::string ReverseStringIterative(std::string str) {
+  int front {0};
+  int back {(int)str.length() - 1};
+
+  while (front < back) {
+    //do something
+    char temp = str[back];
+    str[back] = str[front];
+    str[front] = temp;
+
+    front++;
+    back--;
+  }
+
+  return str;
+}
+
+void ReverseStringRecursive(std::string str) {
   return;
 }
