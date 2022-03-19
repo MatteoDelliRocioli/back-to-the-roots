31dcd040a8687e07ac517f8ccd085c577ff7b67c
Matteo Delli Rocioli <mdellirocioli@altitudo.com>
2022-03-19 22:47:37 +0100

    Implement bubble sort

--- ztm-algos-n-ds/section-13/sorting/src/delli/Main.java
+++ ztm-algos-n-ds/section-13/sorting/src/delli/Main.java
@@ -7,8 +7,31 @@ public class Main {
     char[] letters = {'a', 'd', 'b'};
     int[] numbers = {1, 5, 2};
 
-    javaSort(letters);
-    javaSort(numbers);
+    /*javaSort(letters);
+    javaSort(numbers);*/
+
+    bubbleSort(letters);
+  }
+
+  public static void bubbleSort(char[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] > arr[i + 1]) {
+        System.out.println("i: " + arr[i]);
+        System.out.println("j: " + arr[i + 1]);
+
+        char temp = arr[i + 1];
+        arr[i + 1] = arr[i];
+        arr[i] = temp;
+
+        if (i < arr.length -1) {
+          i++;
+        }
+      }
+    }
+
+    for (char x : arr) {
+      System.out.println(x);
+    }
   }
 
   public static void javaSort(char[] arr) {
