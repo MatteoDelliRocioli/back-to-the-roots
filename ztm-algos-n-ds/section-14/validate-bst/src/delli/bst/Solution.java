46cb1f3f4f557f33a38fe318782b2fe5a6e87605
Matteo Delli Rocioli <mdellirocioli@altitudo.com>
2022-03-27 16:43:05 +0200

    Update validate BST solution

--- ztm-algos-n-ds/section-14/validate-bst/src/delli/bst/Solution.java
+++ ztm-algos-n-ds/section-14/validate-bst/src/delli/bst/Solution.java
@@ -8,6 +8,14 @@ public class Solution {
       return true;
     }
 
+    if (root.left != null && root.left.val == root.val) {
+      return false;
+    }
+
+    if (root.right != null && root.right.val == root.val) {
+      return false;
+    }
+
     return check(root);
   }
 
