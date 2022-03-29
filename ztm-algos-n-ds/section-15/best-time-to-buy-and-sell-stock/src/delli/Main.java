9fe51f638cf5172df0b9960f925f9c906a236573
Matteo Delli Rocioli <mdellirocioli@altitudo.com>
2022-03-29 22:12:52 +0200

    Implement linear solution for maxProfit

--- ztm-algos-n-ds/section-15/best-time-to-buy-and-sell-stock/src/delli/Main.java
+++ ztm-algos-n-ds/section-15/best-time-to-buy-and-sell-stock/src/delli/Main.java
@@ -1,5 +1,8 @@
 package delli;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class Main {
 
   /**
@@ -13,20 +16,50 @@ public class Main {
    * */
   public static void main(String[] args) {
   // write your code here
-    int[] prices = {7,1,5,3,6,4};
-//    int[] prices = {7,6,4,3,1};
+//    int[] prices = {7,1,5,3,6,4};
+    int[] prices = {7,6,4,3,1};
 
-    maxProfit(prices);
+//    maxProfit_BruteForce(prices);
+    int result = maxProfitFinal(prices);
+
+    System.out.println(result);
   }
 
-  public static int maxProfit(int[] prices) {
+  public static int maxProfit_BruteForce(int[] prices) {
+
+    //Map<Integer, Integer, Integer> test = new ConcurrentHashMap<>();
     int maxProfit = 0;
     for (int i = prices.length - 1; i >= 0; i--) {
       for (int z = i - 1; z >= 0; z--) {
         int currentProfit = prices[i] - prices[z];
+        System.out.println(String.format("z+i = %d (profit = %d)", z+i, currentProfit));
         if (currentProfit > maxProfit) {
           maxProfit = currentProfit;
         }
+      }
+    }
+
+    return maxProfit;
+  }
+
+  /**
+   * Linear time complexity
+   * Constant space complexity
+   * */
+  public static int maxProfitFinal(int[] prices) {
+
+    int lowestPrice = prices[0];
+    int maxProfit = 0;
+    int currentProfit = 0;
+
+    for (int i = 0; i < prices.length; i++) {
+      if (prices[i] < lowestPrice) {
+        lowestPrice = prices[i];
+      }
+
+      currentProfit = prices[i] - lowestPrice;
+      if (currentProfit > maxProfit) {
+        maxProfit = currentProfit;
       }
     }
 
