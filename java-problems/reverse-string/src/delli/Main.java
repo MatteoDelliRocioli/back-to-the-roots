0a4f3077eb84a51d00d5cf38056ec5fc5f365f9d
Matteo Delli Rocioli <mdellirocioli@altitudo.com>
2022-03-30 21:53:58 +0200

    Reverse string

--- java-problems/reverse-string/src/delli/Main.java
+++ java-problems/reverse-string/src/delli/Main.java
@@ -13,23 +13,39 @@ public class Main {
    *
    * */
   public static void main(String[] args) {
-    System.out.println(bruteForceApproach("Hi My name is Matteo"));
+//    System.out.println(bruteForceApproach("Hi My name is Matteo"));
+    System.out.println(reverse("Hi My name is Matteo"));
   }
 
   /**
    * Return a copy of the string copying each char from right to left
    * */
   public static String bruteForceApproach(String input) {
-
-    StringBuilder sb = new StringBuilder();
-
     char[] inputChars = input.toCharArray();
     List<Character> resultChars = new ArrayList<>();
 
     for(int i = inputChars.length - 1; i >= 0; i--){
       resultChars.add(inputChars[i]);
     }
-
     return String.valueOf(resultChars);
+  }
+
+  /**
+   * Resolve reversing of string in O(n) time complexity and O(1) space
+   * complexity
+   * */
+  public static String reverse(String input) {
+    char[] charArr = input.toCharArray();
+
+    for (int i = 0; i < charArr.length; i++) {
+      if (i >= charArr.length - 1 - i) {
+        break;
+      }
+      char temp = charArr[i];
+      charArr[i] = charArr[charArr.length - 1 - i];
+      charArr[charArr.length -1 - i] = temp;
+    }
+
+    return String.valueOf(charArr);
   }
 }
