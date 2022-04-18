72c67c1b68bed8fd8690b1bd9aa46df5f56da404
Matteo Delli Rocioli <mdellirocioli@altitudo.com>
2022-04-18 12:42:59 +0200

    Define h index

--- java-problems/h-index/src/delli/Main.java
+++ java-problems/h-index/src/delli/Main.java
@@ -14,10 +14,18 @@ public class Main {
   public static void main(String[] args) {
     int citations[] = {3, 0, 6, 1, 5};
 //    int citations[] = {1, 3, 1};
+//    int citations[] = {100};
+//    int citations[] = {11, 15};
 
+
+//    System.out.println(hIndex(citations));
     System.out.println(hIndex(citations));
   }
 
+  /**
+   * H is the maximum number of articles with at least h citations where this
+   * number of articles is not larger then h
+   * */
   public static int hIndex(int[] citations) {
     Arrays.sort(citations);
     int len = citations.length;
