d5104cb374ad82495dd7b7e0972ec7e198ec47e5
Matteo Delli Rocioli <mdellirocioli@altitudo.com>
2022-05-02 22:56:21 +0200

    Update max-product-subarray with Kadane's algo

--- java-problems/maximum-product-subarray/src/delli/Main.java
+++ java-problems/maximum-product-subarray/src/delli/Main.java
@@ -32,60 +32,7 @@ public class Main {
    * The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
    * */
   public static void main(String[] args) {
-    System.out.println(maxSubArray(new int[] {-3, -4}));
+//    System.out.println(maxSubArray(new int[] {-3, -4}));
   }
 
-  // Kadane's algorithm
-  // We take note only of sums of subarrays that are positive
-  // Once the sum is negative, reset to 0 (zero)
-  // If the current element is greater than the max, re-assign max
-  public static int maxSubArray(int[] nums) {
-    int max = Integer.MIN_VALUE;
-    int currentMax = 0;
-
-    for (int i = 0; i < nums.length; i++) {
-      currentMax += nums[i];
-
-      if (nums[i] > max) {
-        max = nums[i];
-      }
-
-      if (currentMax < 0) {
-        currentMax = 0;
-        continue;
-      }
-
-      max = Math.max(currentMax, max);
-    }
-
-    return max;
-  }
-
-  // Bruteforce solution, TLE
-  // time complexity: O(n^2) -> two nested for loops
-  // space complexity: O(1) -> only few variables with single updating value
-  public static int maxProduct_bruteForce(int[] nums) {
-    int maxProduct = nums[0];
-    for (int i = 0; i < nums.length; i++) {
-      int tempProduct = nums[i];
-
-      // System.out.println("tempProduct_before: " + tempProduct);
-      for (int x = i + 1; x < nums.length; x++) {
-        tempProduct *= nums[x];
-
-        // System.out.println("nums[x]: " + nums[x]);
-        // System.out.println("tempProduct: " + tempProduct);
-
-        if (tempProduct > maxProduct) {
-          maxProduct = tempProduct;
-        }
-      }
-
-      if (nums[i] > maxProduct) {
-        maxProduct = nums[i];
-      }
-    }
-
-    return maxProduct;
-  }
 }
