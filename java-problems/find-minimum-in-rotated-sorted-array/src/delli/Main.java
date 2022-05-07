6441b9698055d34ef821435e2da58ef486df3847
Matteo Delli Rocioli <mdellirocioli@altitudo.com>
2022-05-07 22:51:36 +0200

    Solve find minimum in rotated sorted array

--- java-problems/find-minimum-in-rotated-sorted-array/src/delli/Main.java
+++ java-problems/find-minimum-in-rotated-sorted-array/src/delli/Main.java
@@ -9,8 +9,38 @@ public class Main {
     findMin(new int[] {4,5,6,7,0,1,2});
   }
 
-  // First attempt
+  /**
+   * By using binary search we are able to reduce the time complexity to O(log n)
+   * The space complexity here would be O(m) where m is the number of call stacks
+   * that we have to run
+   * */
   public static int findMin(int[] nums) {
+    if (nums.length < 4) {
+      int min = nums[0];
+      for (int i = 0; i < nums.length; i++) {
+        min = Math.min(min, nums[i]);
+      }
+      return min;
+    }
+
+    int leftPtr = 0;
+    int middlePtr = nums.length / 2;
+    int rightPtr = nums.length - 1;
+
+    if (nums[leftPtr] < nums[middlePtr] && nums[middlePtr] < nums[rightPtr]) { // ordered array
+      return nums[leftPtr];
+    }
+
+    if (nums[leftPtr] >= nums[middlePtr]) { // search left
+      return findMin(Arrays.copyOfRange(nums, leftPtr, middlePtr + 1));
+    }
+    else { // search right
+      return findMin(Arrays.copyOfRange(nums, middlePtr, rightPtr + 1));
+    }
+  }
+
+  // First attempt
+  public static int findMin_01(int[] nums) {
     if (nums.length < 4) {
       int min = nums[0];
       for (int i = 0; i < nums.length; i++) {
