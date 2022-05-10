64a24270beb767b144bbc4525a281e7fad1d87ef
Matteo Delli Rocioli <mdellirocioli@altitudo.com>
2022-05-10 22:43:49 +0200

    Update solve merge two sorted lists

--- java-problems/merge-two-sorted-lists/src/delli/Main.java
+++ java-problems/merge-two-sorted-lists/src/delli/Main.java
@@ -13,11 +13,19 @@ public class Main {
     this.mergeTwoLists(new ListNode(), new ListNode());
   }
 
+  /**
+   * We are copying the elements into a new linkedList so the resulting space
+   * complexity: O(n) and the time complexity is O(n) as well
+   * */
   public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+    if (list1 == null && list2 == null) {
+      return null;
+    }
+
     ListNode pointer = new ListNode();
     ListNode result = pointer;
 
-    while (list1.next != null && list2.next !=null) {
+    while (list1 != null && list2 !=null) {
       int l1Val = list1.val;
       int l2Val = list2.val;
 
@@ -31,24 +39,37 @@ public class Main {
       }
 
       ListNode next = new ListNode();
+      pointer.next = next;
       pointer = next;
     }
 
-    while (list1.next == null && list2.next != null) {
+    while (list1 == null && list2 != null) {
       pointer.val = list2.val;
       list2 = list2.next;
 
+      if (list2 == null) {
+        continue;
+      }
+
       ListNode next = new ListNode();
+      pointer.next = next;
       pointer = next;
     }
 
-    while (list2.next == null && list1.next != null) {
+    while (list2 == null && list1 != null) {
       pointer.val = list1.val;
       list1 = list1.next;
 
+      if (list1 == null) {
+        continue;
+      }
+
       ListNode next = new ListNode();
+      pointer.next = next;
       pointer = next;
     }
+
+    pointer = null;
 
     return result;
   }
