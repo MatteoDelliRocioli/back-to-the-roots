6c0fc1f0ab17937d6cdcc1a7f5f875d767b2b758
Matteo Delli Rocioli <mdellirocioli@altitudo.com>
2022-05-25 22:17:15 +0200

    Implement add binary with bit by bit computation

--- java-problems/add-binary/src/delli/Main.java
+++ java-problems/add-binary/src/delli/Main.java
@@ -5,57 +5,34 @@ public class Main {
   public static void main(String[] args) {
   }
 
-  //WIP
+  /**
+   * time complexity: O(n) where n is the length of the longest of the two string inputs
+   * space complexity: O(n) where n is the length of the longest of the two strings (possibly + 1 if carry == 1 at the end)
+   * */
   public String addBinary(String a, String b) {
-    if (b.length() < a.length()) {
-      return addBinary(b, a);
+    StringBuilder sb = new StringBuilder(); // used to build result string
+
+    int i = a.length() - 1;// we are going backwords to take into account least significant numbers first
+    int j = b.length() - 1;
+
+    int carry = 0; // at first the carry is always 0 (zero)
+
+    while (i >= 0 || j >= 0) { // iterate till both the input strings are iterated
+      int sum = carry; // the sum each time starts with the carry value
+      if (i >= 0) sum += a.charAt(i) - '0'; // first convert the current chars to numbers
+      if (j >= 0) sum += b.charAt(j) - '0'; // ASCII value of '1' is just 1 step forward of '0'
+
+      sb.append(sum % 2);// decide what the result is for the current iteration
+      carry = sum / 2;// decide the value of the carry for the next iteration
+
+      i--;
+      j--;
     }
 
-    StringBuilder sb = new StringBuilder();
-    Character remainder = '0';
-
-    for (int i = a.length() - 1; i >= 0; i--) {
-      int count = 0;
-
-      System.out.println(a.charAt(i));
-      count += a.charAt(i) == '0' ? 0 : 1;
-      count += b.charAt(i) == '0' ? 0 : 1;
-      count += remainder == '0' ? 0 : 1;
-
-      remainder = update(sb, remainder, count);
-    }
-
-    for (int j = b.length() - a.length() - 1; j >= 0; j--) {
-      int count = 0;
-      count += b.charAt(j) == '0' ? 0 : 1;
-      count += remainder == '0' ? 0 : 1;
-
-      remainder = update(sb, remainder, count);
-    }
-
-    if (remainder == '1') {
-      sb.append('1');
+    if (carry != 0) {
+      sb.append(carry);
     }
 
     return sb.reverse().toString();
-  }
-
-  private char update(StringBuilder sb, Character remainder, int count) {
-    switch (count) {
-      case 0:
-        sb.append('0');
-        return '0';
-      case 1:
-        sb.append('1');
-        return '0';
-      case 2:
-        sb.append('0');
-        return '1';
-      case 3:
-        sb.append('1');
-        return '1';
-      default:
-        return '-';
-    }
   }
 }
