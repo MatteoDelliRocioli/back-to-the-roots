55920772be1ebcefee2b4afa93ce4856d9cf53f6
Matteo Delli Rocioli <mdellirocioli@altitudo.com>
2022-05-29 12:34:03 +0200

    First naive solution to roman-to-integer

---
+++ java-problems/roman-to-integer/src/delli/Main.java
@@ -0,0 +1,55 @@
+package delli;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Main main = new Main();
+    main.romanToInt("XX");
+  }
+
+  public int romanToInt(String s) {
+    int current = 0;
+    int sum = 0;
+
+    while(current < s.length() - 1) {
+      int next = parseChar(s.charAt(current + 1));
+      int curr = parseChar(s.charAt(current));
+
+      if (next == curr || next < curr) {
+        sum += curr;
+      }
+      else {
+        sum += next - curr;
+        current++;
+      }
+      current++;
+    }
+
+    if (current != s.length()) {
+      sum += parseChar(s.charAt(current));
+    }
+
+    return sum;
+  }
+
+  public int parseChar(char in) {
+    switch (in) {
+      case 'I':
+        return 1;
+      case 'V':
+        return 5;
+      case 'X':
+        return 10;
+      case 'L':
+        return 50;
+      case 'C':
+        return 100;
+      case 'D':
+        return 500;
+      case 'M':
+        return 1000;
+      default:
+        return 0;
+    }
+  }
+}
