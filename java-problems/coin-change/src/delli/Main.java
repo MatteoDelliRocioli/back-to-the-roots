a668e842b5c360208d061f38691b7dbef780c5ef
Matteo Delli Rocioli <mdellirocioli@altitudo.com>
2022-05-30 22:00:23 +0200

    WIP: solve coin change

---
+++ java-problems/coin-change/src/delli/Main.java
@@ -0,0 +1,43 @@
+package delli;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+  /**
+   * https://leetcode.com/problems/coin-change/
+   * */
+  public static void main(String[] args) {
+  }
+
+  public int coinChange(int[] coins, int amount) { //amount = 0, [1]
+    if (amount == 0) {
+      return 0;
+    }
+
+    // List<Integer> coinsList = Arrays.asList(coins);
+    List<Integer> coinsList = new ArrayList<>();
+
+    for (int i = 0; i < coins.length; i++) {
+      coinsList.add(coins[i]);
+    }
+
+    Collections.sort(coinsList, Collections.reverseOrder());
+
+    int i = 0;
+    int sum = 0;
+    while (amount >= 0 && i < coinsList.size()) {
+      sum += amount / coinsList.get(i);
+      amount %= coinsList.get(i);
+      i++;
+    }
+
+    if (amount > 0) {
+      return -1;
+    }
+
+    return sum;
+  }
+}
