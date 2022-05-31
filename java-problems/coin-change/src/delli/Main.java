998ef2b1ff2052b0a6e46f6b66f217d3c308668d
Matteo Delli Rocioli <mdellirocioli@altitudo.com>
2022-06-01 01:03:29 +0200

    Update coin change comments

--- java-problems/coin-change/src/delli/Main.java
+++ java-problems/coin-change/src/delli/Main.java
@@ -12,32 +12,34 @@ public class Main {
   public static void main(String[] args) {
   }
 
-  public int coinChange(int[] coins, int amount) { //amount = 0, [1]
-    if (amount == 0) {
-      return 0;
-    }
-
-    // List<Integer> coinsList = Arrays.asList(coins);
-    List<Integer> coinsList = new ArrayList<>();
-
-    for (int i = 0; i < coins.length; i++) {
-      coinsList.add(coins[i]);
-    }
-
-    Collections.sort(coinsList, Collections.reverseOrder());
-
-    int i = 0;
-    int sum = 0;
-    while (amount >= 0 && i < coinsList.size()) {
-      sum += amount / coinsList.get(i);
-      amount %= coinsList.get(i);
-      i++;
-    }
-
-    if (amount > 0) {
-      return -1;
-    }
-
-    return sum;
-  }
+  /**
+   * Greedy approach, failing approach
+   * */
+//  public int coinChange(int[] coins, int amount) { //amount = 0, [1]
+//    if (amount == 0) {
+//      return 0;
+//    }
+//
+//    List<Integer> coinsList = new ArrayList<>();
+//
+//    for (int i = 0; i < coins.length; i++) {
+//      coinsList.add(coins[i]);
+//    }
+//
+//    Collections.sort(coinsList, Collections.reverseOrder());
+//
+//    int i = 0;
+//    int sum = 0;
+//    while (amount >= 0 && i < coinsList.size()) {
+//      sum += amount / coinsList.get(i);
+//      amount %= coinsList.get(i);
+//      i++;
+//    }
+//
+//    if (amount > 0) {
+//      return -1;
+//    }
+//
+//    return sum;
+//  }
 }
