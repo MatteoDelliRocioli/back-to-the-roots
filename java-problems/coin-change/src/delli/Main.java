0629dfd6c61ae921756ea1c3daa03b5758b523af
MatteoDelliRocioli <37411225+MatteoDelliRocioli@users.noreply.github.com>
2022-06-02 10:20:16 +0200

    Update coin change with brute force approach
    (recursion)

--- java-problems/coin-change/src/delli/Main.java
+++ java-problems/coin-change/src/delli/Main.java
@@ -10,6 +10,51 @@ public class Main {
    * https://leetcode.com/problems/coin-change/
    * */
   public static void main(String[] args) {
+    Main main = new Main();
+    main.coinChange(new int[] {1,2,5}, 11);
+  }
+  
+  /**
+   * Brute force approach, TLE but correct approach
+   * I'm using recursion in order to check all the possible paths to count the number
+   * of coins we would need to make up the provided amount
+   *
+   * time complexity: is proportional to the number of coins in the exponential way O(S^n) where S 
+   *    is the amount and n is the number of coins
+   * space complexity: O(n) where n is the max height of the recursion stack call
+   */
+  public int coinChange(int[] coins, int amount) {
+    //for each amount we call the same funciton looking for a zero
+    // if more than zero we call the function again
+    // if less than zero it means that we do not consider that path 
+    
+    if (amount == 0) return 0;
+    
+    helper(amount, coins, 0);
+    
+    return result == Integer.MAX_VALUE ? -1: result;
+  }
+  
+  public void helper(int amount, int[] coins, int counter) {
+    
+    for(int i = 0; i < coins.length; i++) {
+      int tmp = amount - coins[i];
+      
+      if (tmp < 0) {
+        continue;
+      }
+      
+      if (tmp == 0) {
+        // check result
+        result = Math.min(result, counter + 1);
+        
+      }
+      
+      if (tmp > 0) {
+        //recursion calling checking for the remaining amount
+        helper(tmp, coins, counter + 1);
+      }
+    }
   }
 
   /**
