c36b4327105e5eb2058c74ea8ddfba58b4130a4e
Matteo Delli Rocioli <mdellirocioli@altitudo.com>
2022-06-12 15:45:32 +0200

    Update counting bits solution

--- java-problems/counting-bits/src/delli/Main.java
+++ java-problems/counting-bits/src/delli/Main.java
@@ -8,30 +8,52 @@ public class Main {
   /**
    * https://leetcode.com/problems/counting-bits/
    *
-   * time complexity: O(n^2) since for each number in the sequence from 1 to n
-   *    we have to loop those numbers in order to build res[i]
-   * space complexity: O(n) where n is the size of res[] -> n+1
+   *
    * */
   public int[] countBits(int n) {
-    int[] res = new int[n + 1];
-    res[0] = 0;
+    int[] res = new int[n+1];
 
-    for (int i = 1; i < res.length; i++) {
-      res[i] = helper(i);
-    }
-    return res;
-  }
+    for (int i = 0; i <= n; i++) {
+      int result = i;
+      int count = 0;
 
-  public int helper(int n) {
-    int sum = 0;
-    while (n>=0) {
-      sum += n%2;
-      n /= 2;
+      while (result >= 1) {
+        if (result % 2 > 0) {
+          count++;
+        }
 
-      if (n == 0) {
-        n--;
+        result = result / 2;
       }
+
+      res[i] = count;
     }
-    return sum;
+
+    return res;
+
+    /**
+     * time complexity: O(n^2) since for each number in the sequence from 1 to n
+     *    we have to loop those numbers in order to build res[i]
+     * space complexity: O(n) where n is the size of res[] -> n+1
+     * */
+//    int[] res = new int[n + 1];
+//    res[0] = 0;
+//
+//    for (int i = 1; i < res.length; i++) {
+//      res[i] = helper(i);
+//    }
+//    return res;
   }
+
+//  public int helper(int n) {
+//    int sum = 0;
+//    while (n>=0) {
+//      sum += n%2;
+//      n /= 2;
+//
+//      if (n == 0) {
+//        n--;
+//      }
+//    }
+//    return sum;
+//  }
 }
