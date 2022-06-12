61ef1e3e058efbd3b745f37862cc280e584ff8c0
Matteo Delli Rocioli <mdellirocioli@altitudo.com>
2022-06-12 11:56:10 +0200

    Solve coin change with bottom up approach

--- java-problems/coin-change/src/delli/Main.java
+++ java-problems/coin-change/src/delli/Main.java
@@ -1,6 +1,7 @@
 package delli;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -13,6 +14,38 @@ public class Main {
     Main main = new Main();
     main.coinChange(new int[] {1,2,5}, 11);
   }
+
+  public int coinChange(int[] coins, int amount) {
+    // starting from 0 we try to see what's the minimum viable solution
+    // for each new number till we reach our amount
+    // that's the bottom up approach
+
+    // so for 1 we check if there is any coin lower than that to try to subtract it
+    // to our amount, if yes and the remaining minus the value of dp at the remaining position
+    // is zero then compare the result with the minimum at current dp position
+
+    // we have to take into account the map with results
+    // the coins we are going to use for each position in dp
+
+    //first we create dp and fill it with max values, since the coins are positive integers
+    // we cannot have less than 1 in coin value thus the max will be amount + 1
+
+    int[] dp = new int[amount + 1];
+    Arrays.fill(dp, amount + 1);
+
+    dp[0] = 0;
+
+    // 2 nested loops since we are going to check each coin for each dp position
+    for (int i = 1; i <= amount; i++) {
+      for (int j = 0; j < coins.length; j++) {
+        if (coins[j] <= i) {
+          dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+        }
+      }
+    }
+
+    return dp[amount] > amount ? -1 : dp[amount];
+  }
   
   /**
    * Brute force approach, TLE but correct approach
@@ -23,39 +56,39 @@ public class Main {
    *    is the amount and n is the number of coins
    * space complexity: O(n) where n is the max height of the recursion stack call
    */
-  public int coinChange(int[] coins, int amount) {
-    //for each amount we call the same funciton looking for a zero
-    // if more than zero we call the function again
-    // if less than zero it means that we do not consider that path 
-    
-    if (amount == 0) return 0;
-    
-    helper(amount, coins, 0);
-    
-    return result == Integer.MAX_VALUE ? -1: result;
-  }
-  
-  public void helper(int amount, int[] coins, int counter) {
-    
-    for(int i = 0; i < coins.length; i++) {
-      int tmp = amount - coins[i];
-      
-      if (tmp < 0) {
-        continue;
-      }
-      
-      if (tmp == 0) {
-        // check result
-        result = Math.min(result, counter + 1);
-        
-      }
-      
-      if (tmp > 0) {
-        //recursion calling checking for the remaining amount
-        helper(tmp, coins, counter + 1);
-      }
-    }
-  }
+//  public int coinChange(int[] coins, int amount) {
+//    //for each amount we call the same funciton looking for a zero
+//    // if more than zero we call the function again
+//    // if less than zero it means that we do not consider that path
+//
+//    if (amount == 0) return 0;
+//
+//    helper(amount, coins, 0);
+//
+//    return result == Integer.MAX_VALUE ? -1: result;
+//  }
+//
+//  public void helper(int amount, int[] coins, int counter) {
+//
+//    for(int i = 0; i < coins.length; i++) {
+//      int tmp = amount - coins[i];
+//
+//      if (tmp < 0) {
+//        continue;
+//      }
+//
+//      if (tmp == 0) {
+//        // check result
+//        result = Math.min(result, counter + 1);
+//
+//      }
+//
+//      if (tmp > 0) {
+//        //recursion calling checking for the remaining amount
+//        helper(tmp, coins, counter + 1);
+//      }
+//    }
+//  }
 
   /**
    * Greedy approach, failing approach
